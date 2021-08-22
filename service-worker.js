8e2c017171d2069bb61fc4954d6a16c45c06ec0e
Timo968 <80679542+Timo968@users.noreply.github.com>
2021-08-22 14:39:19 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+self.addEventListener("install", function (event) {
+    var offlinePage = new Request("offline.html");
+    event.waitUntil(
+        fetch(offlinePage).then(function (response) {
+            return caches.open("pwa-offline").then(function (cache) {
+                console.log("Offline Page während Install gecached" + response.url);
+                return cache.put(offlinePage, response);
+            });
+        }));
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function (error) {
+            console.error("Netzwerkanfragen fehlgeschlagen. Liefere Offline Page " + error);
+            return caches.open("pwa-offline").then(function (cache) {
+                return cache.match("offline.html");
+            });
+        }));
+});
+
+self.addEventListener("refreshOffline", function (response) {
+    return caches.open("pwa-offline").then(function (cache) {
+        console.log("Offline Seite aktualisiert vom refreshOffline event: " + response.url);
+        return cache.put(offlinePage, response);
+    });
+});
+
+self.addEventListener("notificationclose", function (e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+
+    console.log("Closed notification: " + primaryKey);
+});
